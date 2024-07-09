30b81e9655207627a46ced0ca977f324ca95f63d
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2024-07-09 14:27:53 +0600

    initialize

---
+++ README.md
@@ -0,0 +1,7 @@
+# Frontend Template
+- React
+- React DOM
+- Typescript
+- SWC
+- Vite
+- ShadcnUI
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./shadcn/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  )
+}
